ef4e2dd2c551fa8c23f5149413af131869b623f0
Tiger1994 <1300288499@qq.com>
2020-05-07 16:47:51 +0800

    Add spaces

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace restbed;
 using namespace rapidjson;
 
 void get_hotwords(const string key, string &body){
-	Redis *r=new Redis();
+	Redis *r = new Redis();
 	if(!r->connect("127.0.0.1",6379)){
 		fprintf(stdout,"Connect Redis failed");
 		return;
@@ -24,90 +24,77 @@ void get_hotwords(const string key, string &body){
 	vector<string> items;
 	r->zrevrangebyscore(key,items);
 	
-	for(int i=0;i<items.size();i++){
-		body+=items[i];
-		if(i<int(items.size())-1) body+=" ";
+	for(int i = 0; i < items.size(); i++){
+		body += items[i];
+		if(i<int(items.size())-1) body += " ";
 	}
 	fprintf(stdout, "%.*s\n", (int)body.size(),body.data());
 }
 
 void get_method_handler(const shared_ptr<Session> session){
-	const auto request=session->get_request();
-	const string hot_date=request->get_query_parameter("hot");
+	const auto request = session->get_request();
+	const string hot_date = request->get_query_parameter("hot");
 	string body;
 	if(!hot_date.empty()){
 		fprintf(stdout,"%.*s\n",20,"Getting Hot words.");
-		get_hotwords(hot_date,body);
+		get_hotwords(hot_date, body);
 	}
-	session->close(OK,body,{{"Content-Length",::to_string(body.size())}});
+	session->close(OK, body, {{"Content-Length", ::to_string(body.size())}});
 }
 
 
 void post_method_handler(const shared_ptr<Session> session){
-	const auto request=session->get_request();
-	size_t content_size=request->get_header("Content-Length",0);
+	const auto request = session->get_request();
+	size_t content_size = request->get_header("Content-Length", 0);
 	
-	auto handle_body=[request](const shared_ptr<Session> session,const Bytes &body){	
+	auto handle_body = [request](const shared_ptr<Session> session,const Bytes &body){	
 		fprintf(stdout, "%.*s\n", (int)body.size(),body.data());
 		
-		Redis *r=new Redis();
-		if(!r->connect("127.0.0.1",6379)){
-			fprintf(stdout,"Connect Redis failed");
+		Redis *r = new Redis();
+		if(!r->connect("127.0.0.1", 6379)){
+			fprintf(stdout, "Connect Redis failed");
 			return;
 		}
 		
-		int t=time(0)/86400;//In Unix time, 1 day counts 86400.
-		string t_str=to_string(t);
+		int t = time(0)/86400;  //In Unix time, 1 day counts 86400.
+		string t_str = to_string(t);
 		Document doc;
 
 		//Convert unsighed char* to char*
 		char temp[body.size()+1];
-		memcpy(temp,body.data(),body.size()+1);
+		memcpy(temp,body.data(), body.size()+1);
 		string s_temp(temp);
 
-		//string word=doc["keywords"].GetString();
-		//int res=r->zadd(t_str,1,word);
-		//if(res) fprintf(stdout,"%.*s\n",16,"Add Success!");
-		//else fprintf(stdout,"%.*s\n",16,"Add Failed!");
 		doc.Parse(s_temp.c_str());
 		vector<string> words;
-		for(size_t i=0;i<doc["keywords"].Size();i++){
+		for(size_t i = 0; i<doc["keywords"].Size(); i++){
 			words.push_back(doc["keywords"][i].GetString());
 		}
-		r->zadd_words(t_str,words);
-		delete r;r=NULL;
+		r->zadd_words(t_str, words);
+		delete r;
+		r=NULL;
 
-		session->close(OK,"Post complete!",{{"Content-Length","14"},{"Connection","close"}});
+		session->close(OK, "Post complete!", {{"Content-Length", "14"}, {"Connection", "close"}});
 	};
 
-	session->fetch(content_size,handle_body);	
+	session->fetch(content_size, handle_body);	
 }
 
 
 int main(void){
 	cout<<"Start restbed."<<endl;
-	
-	//auto resource_get=make_shared<Resource>();
-	//resource_get->set_path("/hotwords");
-	//resource_get->set_method_handler("GET",get_method_handler);
 
-	//auto resource_post=make_shared<Resource>();
-	//resource_post->set_path("/resource");
-	//resource_post->set_method_handler("POST",post_method_handler);
-
-	auto resource=make_shared<Resource>();
+	auto resource = make_shared<Resource>();
 	resource->set_path("/resource");
-	resource->set_method_handler("POST",post_method_handler);
-	resource->set_method_handler("GET",get_method_handler);
+	resource->set_method_handler("POST", post_method_handler);
+	resource->set_method_handler("GET", get_method_handler);
 
-	auto settings=make_shared<Settings>();
+	auto settings = make_shared<Settings>();
 	settings->set_port(1984);
-	settings->set_default_header("Connection","close");
+	settings->set_default_header("Connection", "close");
 
 	Service service;
 	service.publish(resource);
-	//service.publish(resource_get);
-	//service.publish(resource_post);
 	service.start(settings);
 
 	return EXIT_SUCCESS;
